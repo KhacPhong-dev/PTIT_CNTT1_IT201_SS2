889f9f55515ffa982dc4b42d9b0dedfcccb04e82
KhacPhong-dev <kaito0676@gmail.com>
2025-06-17 07:57:06 +0700

    new

--- PTIT_CNTT1_IT201_SS2_B1.c
+++ PTIT_CNTT1_IT201_SS2_B1.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 int main() {
-    printf("hello world");
-    int num2 = 99;
+    int n,a;
+    printf("nhap vao so luong phan tu cua mang:");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        printf("nhap vao phan tu thu %d cua mang:",i+1);
+
+        scanf("%d", &arr[i]);
+
+    }
+    printf("mang cua ban la: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    int MaxForArr=arr[0];
+    for (int i = 0; i < n; ++i) {
+        if (arr[i]<arr[i+1]) {
+            MaxForArr=arr[i+1];
+
+        }
+
+    }
+    printf("\n");
+    printf("phan tu lon nhat cua mang la: %d",MaxForArr);
+
 }
